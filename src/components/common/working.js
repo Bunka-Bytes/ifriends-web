958d1f2f70b126cc3f418e9318ee2f7d1fdd65ca
Jamilli Gioielli <65102673+jamilligioielli@users.noreply.github.com>
2022-06-20 03:44:21 -0300

    mandando pro heroku 2

--- src/components/common/working.js
+++ src/components/common/working.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Grid } from 'react-flexbox-grid';
+import { useTranslation } from 'react-i18next';
+
 
 import Image from 'react-bootstrap/Image';
 
@@ -8,14 +10,16 @@ import { Button, Result } from 'antd';
 
 const Working = () => {
 	const navigate = useNavigate();
+  	const { t } = useTranslation();
+
 	return (
 		<Result
 			icon={<Image width={360} fluid src="/imgs/work/working.svg" />}
-			title="Em Breve"
-			subTitle="Desculpe, esta página ainda está em desenvolvimento"
+			title={t('working-comming-soon')}
+			subTitle={t('working-comming-soon-label')}
 			extra={
 				<Button type="primary" onClick={() => navigate('/')}>
-					Ir para a página inicial
+					{t('working-comming-soon-home')}
 				</Button>
 			}
 		/>
